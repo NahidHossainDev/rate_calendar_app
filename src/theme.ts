070af66b23543f21ae123273ed9c0743d1dd7642
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-06-25 23:41:23 +0600

    project initilized and mui setup done

---
+++ src/theme.ts
@@ -0,0 +1,48 @@
+"use client";
+import { Shadows, createTheme } from "@mui/material/styles";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+	weight: ["300", "400", "500", "700"],
+	subsets: ["latin"],
+	display: "swap",
+});
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#76C4EB",
+		},
+		secondary: {
+			main: "#B0B0B0",
+		},
+		background: {
+			default: "#F4F8FA",
+		},
+		grey: {
+			300: "#E1E1E1",
+		},
+	},
+	typography: {
+		fontFamily: roboto.style.fontFamily,
+	},
+	shadows: [
+		"none",
+		"0px 0px 0px 0px rgba(0, 0, 0, 0)",
+		"0px 2px 2px 0px rgba(0, 0, 0, 0.1)",
+		...Array(23).fill("0px 3px 20px 0px rgba(0, 0, 0, 0.1)"),
+	] as Shadows,
+	components: {
+		MuiAlert: {
+			styleOverrides: {
+				root: ({ ownerState }) => ({
+					...(ownerState.severity === "info" && {
+						backgroundColor: "#60a5fa",
+					}),
+				}),
+			},
+		},
+	},
+});
+
+export default theme;
