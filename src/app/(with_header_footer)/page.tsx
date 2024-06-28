877fbcb7425d13968ac3566c3c39e2e6284a08ec
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-06-28 19:34:36 +0600

    DateRange Calender fixed

--- src/app/(with_header_footer)/page.tsx
+++ src/app/(with_header_footer)/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@mui/material";
+import { SingleInputDateRangePicker } from "@/components/molecules";
+import { Paper, Stack, Typography } from "@mui/material";
 
 export default function Home() {
 	return (
-		<main>
-			<Button variant='contained'>Button</Button>
-		</main>
+		<Stack spacing={2} p={2}>
+			<Paper sx={{ p: 3, borderRadius: 2 }}>
+				<Typography variant="h6" fontWeight={600}>
+					Rate Calendar App
+				</Typography>
+				<SingleInputDateRangePicker />
+			</Paper>
+
+			<Paper>Button</Paper>
+		</Stack>
 	);
 }
+
--- src/components/molecules/SingleInputDateRangePicker.tsx
+++ src/components/molecules/SingleInputDateRangePicker.tsx
@@ -1,15 +1,28 @@
+"use client";
+import { styled } from "@mui/system";
 import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 import { SingleInputDateRangeField } from "@mui/x-date-pickers-pro/SingleInputDateRangeField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 
-export default function SingleInputDateRangePicker() {
+const StyledDemoContainer = styled(DemoContainer)({
+	"& > :first-child": {
+		display: "none",
+	},
+	width: 100,
+});
+
+export function SingleInputDateRangePicker() {
 	return (
 		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<DemoContainer components={["SingleInputDateRangeField"]}>
-				<DateRangePicker slots={{ field: SingleInputDateRangeField }} name='allowedRange' />
-			</DemoContainer>
+			<StyledDemoContainer components={["SingleInputDateRangeField"]}>
+				<DateRangePicker
+					sx={{ py: 0 }}
+					slots={{ field: SingleInputDateRangeField }}
+					name="allowedRange"
+				/>
+			</StyledDemoContainer>
 		</LocalizationProvider>
 	);
 }
