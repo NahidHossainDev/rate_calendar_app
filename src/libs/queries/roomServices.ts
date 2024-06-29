1e7773f9504d3f7675696b10c741af671ae7d16a
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-06-29 11:10:04 +0600

    useRoomCalender query hook created

---
+++ src/libs/queries/roomServices.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { ReadonlyURLSearchParams } from "next/navigation";
+import { roomServiceApi } from "../api";
+
+export const useRoomCalender = (query: ReadonlyURLSearchParams) =>
+	useQuery({
+		queryKey: ["room"],
+		queryFn: () => {
+			const finalQuery = new URLSearchParams(query);
+			if (!finalQuery.has("start_date") || !finalQuery.has("end_date")) {
+				const today = new Date().toISOString().slice(0, 10);
+				if (!finalQuery.has("start_date")) finalQuery.set("start_date", today);
+				if (!finalQuery.has("end_date")) finalQuery.set("end_date", today);
+			}
+			return roomServiceApi.getRoomCalender(finalQuery.toString());
+		},
+	});
